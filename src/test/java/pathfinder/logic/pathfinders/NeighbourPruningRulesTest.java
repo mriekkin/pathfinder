a66f080cc75370514deef592f250107a6223631e
Matti Riekkinen <matti.riekkinen@icloud.com>
2018-08-02 12:04:26 +0300

    Additional tests for neighbour pruning rules

--- src/test/java/pathfinder/logic/pathfinders/NeighbourPruningRulesTest.java
+++ src/test/java/pathfinder/logic/pathfinders/NeighbourPruningRulesTest.java
@@ -35,12 +35,12 @@ public class NeighbourPruningRulesTest {
         JumpPointSearch pathfinder = new JumpPointSearch(g);
         Node p = g.getNode(4, 5);
         Node x = g.getNode(5, 5);
-        g.getNode(5, 4).setWalkable(false); // Add obstacles
-        g.getNode(5, 6).setWalkable(false);
+        g.getNode(5, 4).setWalkable(false); // Add obstacles,
+        g.getNode(5, 6).setWalkable(false); // which should create forced neighbours
         List<Node> neighbours = pathfinder.getPrunedNeighbours(p, x);
         assertEquals(3, neighbours.size());
-        assertEquals("(6, 5)", neighbours.get(0).toString());
-        assertEquals("(6, 4)", neighbours.get(1).toString());
+        assertEquals("(6, 5)", neighbours.get(0).toString()); // 1 natural neighbour
+        assertEquals("(6, 4)", neighbours.get(1).toString()); // 2 forced neighbours
         assertEquals("(6, 6)", neighbours.get(2).toString());
     }
 
@@ -53,18 +53,18 @@ public class NeighbourPruningRulesTest {
         assertEquals(1, neighbours.size());
         assertEquals("(5, 6)", neighbours.get(0).toString());
     }
-    
+
     @Test
     public void returnsForcedNeighboursForVerticalMove() {
         JumpPointSearch pathfinder = new JumpPointSearch(g);
         Node p = g.getNode(5, 4);
         Node x = g.getNode(5, 5);
-        g.getNode(4, 5).setWalkable(false); // Add obstacles
-        g.getNode(6, 5).setWalkable(false);
+        g.getNode(4, 5).setWalkable(false); // Add obstacles,
+        g.getNode(6, 5).setWalkable(false); // which should create forced neighbours
         List<Node> neighbours = pathfinder.getPrunedNeighbours(p, x);
         assertEquals(3, neighbours.size());
-        assertEquals("(5, 6)", neighbours.get(0).toString());
-        assertEquals("(4, 6)", neighbours.get(1).toString());
+        assertEquals("(5, 6)", neighbours.get(0).toString()); // 1 natural neighbour
+        assertEquals("(4, 6)", neighbours.get(1).toString()); // 2 forced neighbours
         assertEquals("(6, 6)", neighbours.get(2).toString());
     }
 
@@ -79,21 +79,57 @@ public class NeighbourPruningRulesTest {
         assertEquals("(6, 4)", neighbours.get(1).toString());
         assertEquals("(6, 5)", neighbours.get(2).toString());
     }
-    
+
     @Test
     public void returnsForcedNeighboursForDiagonalMove() {
         JumpPointSearch pathfinder = new JumpPointSearch(g);
         Node p = g.getNode(4, 6);
         Node x = g.getNode(5, 5);
-        g.getNode(4, 5).setWalkable(false); // Add obstacles
-        g.getNode(5, 6).setWalkable(false);
+        g.getNode(4, 5).setWalkable(false); // Add obstacles,
+        g.getNode(5, 6).setWalkable(false); // which should create forced neighbours
         List<Node> neighbours = pathfinder.getPrunedNeighbours(p, x);
         assertEquals(5, neighbours.size());
-        assertEquals("(5, 4)", neighbours.get(0).toString()); // Natural (3)
+        assertEquals("(5, 4)", neighbours.get(0).toString()); // 3 natural neighbours
         assertEquals("(6, 4)", neighbours.get(1).toString());
         assertEquals("(6, 5)", neighbours.get(2).toString());
-        assertEquals("(4, 4)", neighbours.get(3).toString()); // Forced (2)
+        assertEquals("(4, 4)", neighbours.get(3).toString()); // 2 forced neighbours
         assertEquals("(6, 6)", neighbours.get(4).toString());
+    }
+
+    @Test
+    public void doesNotReturnUnwalkableNaturalNeighbours() {
+        JumpPointSearch pathfinder = new JumpPointSearch(g);
+        Node p = g.getNode(4, 5); // Horizontal move
+        Node x = g.getNode(5, 5);
+        g.getNode(6, 5).setWalkable(false); // Block the 1 natural neighbour
+        List<Node> neighbours = pathfinder.getPrunedNeighbours(p, x);
+        assertTrue(neighbours.isEmpty());
+    }
+
+    @Test
+    public void doesNotReturnUnwalkableForcedNeighbours() {
+        JumpPointSearch pathfinder = new JumpPointSearch(g);
+        Node p = g.getNode(4, 5); // Horizontal move
+        Node x = g.getNode(5, 5);
+        g.getNode(5, 4).setWalkable(false); // Add obstacles to get 2 forced neighbours
+        g.getNode(5, 6).setWalkable(false);
+        g.getNode(6, 4).setWalkable(false); // But make them unwalkable
+        g.getNode(6, 6).setWalkable(false);
+        List<Node> neighbours = pathfinder.getPrunedNeighbours(p, x);
+        assertEquals(1, neighbours.size());
+        assertEquals("(6, 5)", neighbours.get(0).toString()); // Only the 1 natural neighbour remains
+    }
+
+    @Test
+    public void irrelevantObstaclesDoNotCreateForcedNeighbours() {
+        JumpPointSearch pathfinder = new JumpPointSearch(g);
+        Node p = g.getNode(4, 5); // Horizontal move
+        Node x = g.getNode(5, 5);
+        g.getNode(4, 4).setWalkable(false); // Block all "irrelevant" neighbours,
+        g.getNode(4, 6).setWalkable(false); // which should not create a forced neighbour
+        List<Node> neighbours = pathfinder.getPrunedNeighbours(p, x);
+        assertEquals(1, neighbours.size()); // No forced neighbours (only the 1 natural neighbour)
+        assertEquals("(6, 5)", neighbours.get(0).toString());
     }
 
 }
