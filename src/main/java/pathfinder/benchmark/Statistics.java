6509e5b4d1b41f04ca13350d77c76c7414be4a29
Matti Riekkinen <matti.riekkinen@icloud.com>
2018-08-29 10:36:21 +0300

    Compute median with self-implemented insertion sort

--- src/main/java/pathfinder/benchmark/Statistics.java
+++ src/main/java/pathfinder/benchmark/Statistics.java
@@ -1,21 +1,17 @@
 package pathfinder.benchmark;
 
-import java.util.Arrays;
-
 /**
  * Computation of selected statistical measures.
  */
 public class Statistics {
 
     /**
-     * Returns the median of the specified array of longs.
+     * Returns the median of the specified array of doubles.
      *
      * @param x array whose median is to be computed
      * @return the median of the specified array
      */
-    public static final double median(long[] x) {
-        Arrays.sort(x);
-
+    public static final double median(double[] x) {
         final int n = x.length;
         if (n % 2 == 0) {
             return (x[n/2] + x[n/2 - 1]) / 2.0;
@@ -25,19 +21,25 @@ public class Statistics {
     }
 
     /**
-     * Returns the median of the specified array of doubles.
-     *
-     * @param x array whose median is to be computed
-     * @return the median of the specified array
+     * Sorts the specified array into ascending order, according to the natural
+     * ordering of its elements.
+     * <p>
+     * This method implements insertion sort. Insertion sort was chosen because
+     * it's simple and sufficiently fast for very short arrays. The arrays
+     * to be sorted by this method are expected to be of length 10 or less.
+     * 
+     * @param a the array to be sorted
      */
-    public static final double median(double[] x) {
-        Arrays.sort(x);
+    protected static void sort(double[] a) {
+        for (int i = 1; i < a.length; i++) {
+            double x = a[i];
+            int j = i-1;
+            while (j >= 0 && a[j] > x) {
+                a[j+1] = a[j];
+                j--;
+            }
 
-        final int n = x.length;
-        if (n % 2 == 0) {
-            return (x[n/2] + x[n/2 - 1]) / 2.0;
-        } else {
-            return x[n/2];
+            a[j+1] = x;
         }
     }
 
--- src/test/java/pathfinder/benchmark/StatisticsTest.java
+++ src/test/java/pathfinder/benchmark/StatisticsTest.java
@@ -6,27 +6,22 @@ import static org.junit.Assert.*;
 public class StatisticsTest {
 
     @Test
-    public void computesMedianForOddNumberOfLongs() {
-        long[] x = new long[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        assertEquals(5, Statistics.median(x), 0.000001);
-    }
-
-    @Test
     public void computesMedianForOddNumberOfDoubles() {
-        long[] x = new long[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        double[] x = new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
         assertEquals(5, Statistics.median(x), 0.000001);
-    }
-
-    @Test
-    public void computesMedianForEvenNumberOfLongs() {
-        long[] x = new long[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        assertEquals(5.5, Statistics.median(x), 0.000001);
     }
 
     @Test
     public void computesMedianForEvenNumberOfDoubles() {
-        long[] x = new long[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double[] x = new double[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         assertEquals(5.5, Statistics.median(x), 0.000001);
+    }
+
+    @Test
+    public void sortsArrayInAscendingOrder() {
+        double[] x = new double[]{3, 3, 2, 5, 7, 3, 0, 9, 8, 5, 1, 4, 6, 10};
+        Statistics.sort(x);
+        assertArrayEquals(new double[]{0, 1, 2, 3, 3, 3, 4, 5, 5, 6, 7, 8, 9, 10}, x, 0.000001);
     }
 
     @Test
